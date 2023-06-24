9d4f5f4c2562a7e75cd2cce71df16a081d0557ae
Tao_7651 <origin7651@outlook.com>
2023-06-24 23:56:40 +0800

    start searching news, no filter

--- service/news.go
+++ service/news.go
@@ -11,12 +11,28 @@ func AddOneNews(form model.News) (news model.News, err error) {
 	return form, err
 }
 
-func GetSomeNews(form model.GetSomeNews) (newsList []model.News, err error) {
+type NewsResult struct {
+	model.News
+	Category1  bool
+	Category2  bool
+	Category3  bool
+	Category4  bool
+	Category5  bool
+	Category6  bool
+	Category7  bool
+	Category8  bool
+	Category9  bool
+	Category10 bool
+}
+
+func GetSomeNews(form model.GetSomeNews) (newsList []NewsResult, err error) {
 	res := model.DB.
+		Model(&model.News{}).
+		Select("news.id", "news.text", "categories.Category1").
 		Limit(form.Limit).
-		Offset((form.Page - 1) * form.Limit).
-		Joins("INNER JOIN categories ON categories.foreign_key = news.id").
-		Find(&newsList)
+		Offset((form.Page-1)*form.Limit).
+		Joins("INNER JOIN categories ON categories.foreign_key = news.id AND categories.tablee = ?", "News").
+		Scan(&newsList)
 	err = res.Error
 	return
 }
