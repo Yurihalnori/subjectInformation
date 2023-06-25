2500a88487bd0fc7d66d2b907acec8b15bb22907
Tao_7651 <origin7651@outlook.com>
2023-06-25 20:20:31 +0800

    start searching news, no filter

--- service/news.go
+++ service/news.go
@@ -1,37 +1,30 @@
 package service
 
 import (
+	"strconv"
 	"subjectInformation/model"
 )
 
 func AddOneNews(form model.News) (news model.News, err error) {
-	res := model.DB.Select("title", "module", "region", "department", "date", "text").Create(&form)
+	res := model.DB.Select("title", "module", "region", "department", "date", "text", "category").Create(&form)
 	err = res.Error
 	Category{}.AddCategory(form.Category, form.Id, "News")
 	return form, err
 }
 
-type NewsResult struct {
-	model.News
-	Category1  bool
-	Category2  bool
-	Category3  bool
-	Category4  bool
-	Category5  bool
-	Category6  bool
-	Category7  bool
-	Category8  bool
-	Category9  bool
-	Category10 bool
-}
-
-func GetSomeNews(form model.GetSomeNews) (newsList []NewsResult, err error) {
+func GetSomeNews(form model.GetSomeNews) (newsList []model.News, err error) {
+	condition := "INNER JOIN categories ON categories.foreign_key = news.id AND categories.tablee = 'News' AND( "
+	for key, value := range form.Category {
+		if value == 49 { // ascii 1 = 49
+			condition += "categories.category" + strconv.Itoa(key+1) + " ='1'" + " OR "
+		}
+	}
+	condition += "1=0)"
 	res := model.DB.
 		Model(&model.News{}).
-		Select("news.id", "news.text", "categories.Category1").
 		Limit(form.Limit).
-		Offset((form.Page-1)*form.Limit).
-		Joins("INNER JOIN categories ON categories.foreign_key = news.id AND categories.tablee = ?", "News").
+		Offset((form.Page - 1) * form.Limit).
+		Joins(condition).
 		Scan(&newsList)
 	err = res.Error
 	return
